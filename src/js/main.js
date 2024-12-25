25d403ffaca9b800c483253a1c6dc81a363cb801
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-12-25 22:28:13 +0400

    feat: add dark-light theme

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,6 @@
 import "modern-normalize/modern-normalize.css";
 import "../scss/style.scss";
+
+import { initDarkLightTheme } from "./modules/theme";
+
+initDarkLightTheme();
---
+++ src/js/modules/theme.js
@@ -0,0 +1,27 @@
+export function initDarkLightTheme() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const themeToggleButton = document.querySelector(".sun-moon");
+    const body = document.body;
+
+    // Установить начальную тему
+    const userPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme) {
+      body.setAttribute("data-theme", savedTheme);
+    } else {
+      body.setAttribute("data-theme", userPrefersDark ? "dark" : "light");
+    }
+
+    // Переключить тему
+    themeToggleButton.addEventListener("click", () => {
+      const currentTheme = body.getAttribute("data-theme");
+      const newTheme = currentTheme === "dark" ? "light" : "dark";
+      body.setAttribute("data-theme", newTheme);
+      localStorage.setItem("theme", newTheme);
+    });
+  });
+}
+
