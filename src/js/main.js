3ab8bfd54ac5cf763e93446e0d715e9c5462d326
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-12-28 22:02:57 +0400

    feat: add main page animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,7 @@ import "modern-normalize/modern-normalize.css";
 import "../scss/style.scss";
 
 import { initDarkLightTheme } from "./modules/theme";
+import { initAnimation } from "./modules/animation";
 
 initDarkLightTheme();
+initAnimation();
---
+++ src/js/modules/animation.js
@@ -0,0 +1,20 @@
+export function initAnimation() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const typewriter = document.getElementById("typewriter");
+    const sections = document.querySelectorAll(".main, .footer");
+
+    typewriter.addEventListener("animationstart", () => {
+      typewriter.style.opacity = "1";
+      typewriter.style.visibility = "visible";
+    });
+
+    typewriter.addEventListener("animationend", () => {
+      typewriter.classList.add("finished");
+      sections.forEach((section) => {
+        section.style.opacity = "1";
+        section.style.visibility = "visible";
+        section.classList.add("finished");
+      });
+    });
+  });
+}
