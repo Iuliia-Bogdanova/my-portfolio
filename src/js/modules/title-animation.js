f5cc1899971156a38d97bf9b48d1d11bde443056
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2025-05-18 16:30:44 +0400

    feat: add error-img

--- src/js/modules/title-animation.js
+++ src/js/modules/title-animation.js
@@ -1,28 +1,28 @@
 import { isCurrentPage } from "./utils";
 
 export function initTitleAnimation() {
-  if (!isCurrentPage("home-page")) return;
+    if (!isCurrentPage("home-page")) return;
 
-  // Ждать загрузки DOM для запуска анимации
-  document.addEventListener("DOMContentLoaded", () => {
-    const typewriter = document.getElementById("typewriter");
-    const sections = document.querySelectorAll(".main, .footer");
+    // Ждать загрузки DOM для запуска анимации
+    document.addEventListener("DOMContentLoaded", () => {
+        const typewriter = document.getElementById("typewriter");
+        const sections = document.querySelectorAll(".main, .footer");
 
-    // Проверить, есть ли элемент для анимации
-    if (typewriter) {
-      typewriter.style.opacity = "1";
-      typewriter.style.visibility = "visible";
-    }
+        // Проверить, есть ли элемент для анимации
+        if (typewriter) {
+            typewriter.style.opacity = "1";
+            typewriter.style.visibility = "visible";
+        }
 
-    // Обработать окончание анимации
-    typewriter.addEventListener("animationend", () => {
-      document.body.style.overflow = "auto";
-      typewriter.classList.add("finished");
-      sections.forEach((section) => {
-        section.style.opacity = "1";
-        section.style.visibility = "visible";
-        section.classList.add("finished");
-      });
+        // Обработать окончание анимации
+        typewriter.addEventListener("animationend", () => {
+            document.body.style.overflow = "auto";
+            typewriter.classList.add("finished");
+            sections.forEach((section) => {
+                section.style.opacity = "1";
+                section.style.visibility = "visible";
+                section.classList.add("finished");
+            });
+        });
     });
-  });
 }
